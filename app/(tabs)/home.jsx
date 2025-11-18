dedca53b49077da30eb7e9b678164663dbfb4802
reginadokponou27 <regina.dokponou@epitech.eu>
2025-11-18 16:20:24 +0100

    refactor: change back home to initial design after test

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,23 @@
-import { View } from "react-native-web";
-import KanbanView from "../../components/Kanban";
+import { View, Text } from "react-native-web";
 
 
 export default function HomeScreen() {
   return (
-    <View className="w-80 max-h-screen">
-      <KanbanView />
-    </View>
+    <View className="flex-1 bg-[#1a1a1a]">
+      {/* Header */}
+      <View className="bg-[#2a2a2a] px-4 pt-12 pb-6 border-b border-gray-700">
+        <Text className="text-2xl font-bold text-white">
+          Workspaces
+        </Text>
+      </View>
+
+      {/* Content */}
+      <View className="flex-1 items-center justify-center">
+        <Text className="text-gray-400 text-lg">
+          No workspaces yet
+        </Text>
+      </View>
+      </View>
     
    
   );
